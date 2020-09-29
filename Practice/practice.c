91e41fb6152421d8b746161bbe92755def9db9c5
Ryan Christopher <ryanchristopher27@users.noreply.github.com>
2020-09-28 19:39:58 -0500

    Practice Updates
    Practice Updates in CS 3050

---
+++ Practice/practice.c
@@ -0,0 +1,15 @@
+// Ryan Christopher
+// Practice C File
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Hello World!");
+    int i = 0;
+    if (i == 2) {
+        printf("\nI does not equal 0\n");
+    } else {
+        printf("\nI equals 0\n");
+    }
+}
